1e4af0fc03c48aec924d9bb1a7c46e036444a23d
Carlos Guilherme <carlosguilherme.schneider@gmail.com>
2021-04-24 21:30:56 -0300

    Exception handling

--- src/main/java/com/carlosg/workshopmongo/config/Instantiation.java
+++ src/main/java/com/carlosg/workshopmongo/config/Instantiation.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
 
 import com.carlosg.workshopmongo.domain.User;
 import com.carlosg.workshopmongo.repository.UserRepository;
 
+@Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
@@ -21,8 +23,9 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User silvio = new User(null, "Silvio Dias", "dias.s86@gmail.com");
 	
-		userRepository.saveAll( Arrays.asList(maria, alex, bob));
+		userRepository.saveAll( Arrays.asList(maria, alex, bob, silvio));
 	}
 
 }
--- src/main/java/com/carlosg/workshopmongo/resources/UserResource.java
+++ src/main/java/com/carlosg/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.carlosg.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carlosg.workshopmongo.domain.User;
+import com.carlosg.workshopmongo.dto.UserDTO;
 import com.carlosg.workshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +22,15 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> allUsers = service.findAll();
-		return ResponseEntity.ok().body(allUsers);
+		List<UserDTO> allUsersDTO = allUsers.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(allUsersDTO);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User thisUser = service.findById( id );
+		return ResponseEntity.ok().body(new UserDTO(thisUser));
 	}
 }
